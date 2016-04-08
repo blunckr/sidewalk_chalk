c98bac2cecafddeaba9070e7b8c64cbdf01868bc
Ryker Blunck <rykerblunck@gmail.com>
2016-04-08 15:40:45 -0600

    added react. drawing works

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,11 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Canvas from "./canvas"
+
+ReactDOM.render(
+  <Canvas/>,
+  document.getElementById("app")
+)
---
+++ web/static/js/canvas.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import _ from 'lodash';
+
+export default class Canvas extends React.Component{
+  constructor(){
+    super();
+    var color = '#000000';
+
+    var grid = [];
+    _.times(5, ()=> grid.push([]));
+    _.each(grid, row => {
+      _.times(5, ()=> row.push({color: '#FFFFFF'}));
+    });
+
+    this.state = {color, grid, mouseDown: false};
+  }
+
+  changeColor(color){
+    this.setState({color})
+  }
+
+  blockClick(x, y){
+    this.blockChange(x, y);
+  }
+
+  blockEnter(x, y){
+    if(this.state.mouseDown){
+      this.blockChange(x, y);
+    }
+  }
+
+  blockChange(x, y){
+    this.state.grid[x][y].color = this.state.color;
+    this.setState({grid: this.state.grid});
+  }
+
+
+  startColor(){
+    this.setState({mouseDown: true});
+  }
+
+  stopColor(){
+    this.setState({mouseDown: false});
+  }
+
+  render(){
+    return(
+      <div>
+        <svg width="500" height="500"
+          onMouseDown= {()=>this.startColor()}
+          onMouseUp=   {()=>this.stopColor()}
+          onMouseLeave={()=>this.stopColor()}>
+          {_.map(this.state.grid, (row, x) =>
+            _.map(row, (block, y) =>
+              <rect
+                key={`x${x}y${y}`}
+                x={x * 100} y={y * 100}
+                width="100" height="100"
+                style={{fill: block.color}}
+                onMouseDown= {()=> this.blockClick(x, y)}
+                onMouseEnter={()=> this.blockEnter(x, y)}/>
+            )
+          )}
+        </svg>
+        <br />
+        <input type="color" value={this.state.color} onChange={(e)=> this.changeColor(e.target.value)}/>
+      </div>
+    );
+  }
+};
