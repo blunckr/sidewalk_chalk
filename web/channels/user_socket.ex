b99c70b4ea7dbf6a4056115767c3dba96b6da772
Ryker Blunck <rykerblunck@gmail.com>
2016-04-24 19:15:51 -0600

    ready for heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,8 @@ defmodule SidewalkChalk.UserSocket do
   channel "sidewalks:*", SidewalkChalk.SidewalkChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
